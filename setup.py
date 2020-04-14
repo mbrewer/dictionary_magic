e5495b0ac49f62f437f0e1b462bf65b2c713c71b
Mallory Brewer <maloret@gmail.com>
2020-04-14 10:40:54 -0400

    upload distribution archives

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="dictionaries-maloret", # Replace with your own username
+    version="1.0.0",
+    author="Mallory Brewer",
+    author_email="maloret@gmail.com",
+    description="A small dictionary program",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mbrewer/dictionary_magic",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
